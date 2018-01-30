51d46c75ade861f3ef33b2314e88979f5a5fc324
Dinusha <kumarasiri_048@hotmail.com>
2018-01-30 23:44:03 +0530

    Initial Commit

---
+++ src/webparts/spFxTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxTestWebPartStrings' {
+  const strings: ISpFxTestWebPartStrings;
+  export = strings;
+}
