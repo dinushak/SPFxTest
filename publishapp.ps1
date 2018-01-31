3505b3ba8b568f1ecfc07b633efada28f158d510
Dinusha <kumarasiri_048@hotmail.com>
2018-01-31 20:04:23 +0530

    add content to ps1

---
+++ publishapp.ps1
@@ -0,0 +1,9 @@
+$packagePath = "$(System.DefaultWorkingDirectory)\release\sharepoint\solution\sp-fx-test.sppkg"
+$encpassword = convertto-securestring -String $(Password) -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $(Username), $encpassword
+Connect-PnPOnline -Url "https://dinushaonline.sharepoint.com/sites/dev" -Credentials $cred
+Add-PnPApp -Path $packagePath -Publish -Overwrite
+
+$appAfterAdd = Get-PnPApp | ? { $_.Title -eq 'SPFxTest' }
+Publish-PnPApp -Identity $appAfterAdd.id
+Install-PnPApp -Identity $appAfterAdd.id
