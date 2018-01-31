51e93f9092f9750a3666928f3cb357f16d590112
Dinusha <kumarasiri_048@hotmail.com>
2018-01-31 23:10:13 +0530

    f

--- publishapp.ps1
+++ publishapp.ps1
@@ -1,7 +1,19 @@
+Param(
+    [string]$username,
+    [string]$password,
+)
+
 $packagePath = ".\release\sharepoint\solution\sp-fx-test.sppkg"
-$encpassword = convertto-securestring -String $(Password) -AsPlainText -Force
-$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $(Username), $encpassword
+$encpassword = convertto-securestring -String $password -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $encpassword
 Connect-PnPOnline -Url "https://dinushaonline.sharepoint.com/sites/dev" -Credentials $cred
+
+$appExist = Get-PnPApp | ? { $_.Title -eq 'SPFxTest' }
+if ($appExist -ne $null) {	
+    Uninstall-PnPApp -Identity $appExist.Id		
+	Remove-PnPApp -Identity $appExist.Id
+}
+
 Add-PnPApp -Path $packagePath -Publish -Overwrite
 
 $appAfterAdd = Get-PnPApp | ? { $_.Title -eq 'SPFxTest' }
